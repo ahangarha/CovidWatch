49dad290f5df0fa5cb74085b5bb468d443a5029d
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-12 14:07:36 +0300

    add basic routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import AllCountries from './pages/AllCountries';
+import CountryPage from './pages/CountryPage';
+
 function App() {
   return (
-    <div className="App">
-      the app
+    <div className="min-h-screen bg-pink-400 text-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<AllCountries />} />
+        <Route path="/countries/:countryName" element={<CountryPage />} />
+        <Route
+          path="*"
+          element={(
+            <main className="p-4">
+              <p>Not exist!</p>
+            </main>
+          )}
+        />
+      </Routes>
     </div>
   );
 }
