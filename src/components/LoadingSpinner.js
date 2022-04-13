4198b71d609c5ddc64b7a7ac434802b764603b3c
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-13 13:36:18 +0300

    redesing loading spinner

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LoadingSpinner = () => (
-  <div className="inline-block w-4 h-4 border-b-2 border-t-2 rounded-full animate-spin" />
+  <div className="inline-block w-6 h-6 border-white border-2 border-t-white/0 border-r-white/25 border-b-white/60 rounded-full animate-spin" />
 );
 
 export default LoadingSpinner;
