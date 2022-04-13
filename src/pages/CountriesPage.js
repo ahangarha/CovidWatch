1cb20e3707868457f11bdfd2c6791ded0716e944
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-13 21:53:18 +0300

    improve header

--- src/pages/CountriesPage.js
+++ src/pages/CountriesPage.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import CountriesList from '../components/CountriesList';
+import { getMapUrl } from '../modules/mapUtils';
 
 function AllCountries() {
+  const imageUrl = getMapUrl('world');
+
   return (
     <main className="container mx-auto flex flex-col grow">
-      <div className="h-48 grid place-content-center">
-        Global Statistics
+      <div className="h-48 md:h-64 grid place-content-center relative text-center">
+        <div
+          className="absolute inset-0 z-0 opacity-10"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          }}
+        />
+        <h2 className="z-10 text-center text-3xl">
+          Daily statistics of confirmed
+          <br />
+          Covid-19 cases
+        </h2>
+        <p className="text-xs inline-block bg-black/10 font-bold">
+          Source: John Hopkins University
+        </p>
       </div>
       <div className="bg-pink-500 grow rounded-t-3xl border-t border-t-pink-600">
         <div className="p-2 text-center uppercase font-bold">State by countries</div>
