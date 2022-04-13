b83ebaff5597bd3e5585980f9845520ffc2d64c7
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-13 22:33:15 +0300

    make size of loadingSpinner configurable

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
-const LoadingSpinner = () => (
-  <div className="inline-block w-6 h-6 border-white border-2 border-t-white/0 border-r-white/25 border-b-white/60 rounded-full animate-spin" />
-);
+const LoadingSpinner = ({ size = '' }) => {
+  const sizeClasses = size.length ? size : 'w-6 h-6';
+  console.log(size);
+  return (
+    <div className={`inline-block ${sizeClasses} border-white border-2 border-t-white/0 border-r-white/25 border-b-white/60 rounded-full animate-spin`} />
+  );
+};
+
+LoadingSpinner.propTypes = {
+  size: PropTypes.string,
+};
+
+LoadingSpinner.defaultProps = {
+  size: '',
+};
 
 export default LoadingSpinner;
