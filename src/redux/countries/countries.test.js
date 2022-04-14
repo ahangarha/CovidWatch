501baf9e2703dc7cb6aa01898454313b295b5c13
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-15 00:25:40 +0300

    add test for success action and fetchAllData

--- src/redux/countries/countries.test.js
+++ src/redux/countries/countries.test.js
@@ -1,4 +1,47 @@
-import regionReducer, { fetchDataFailure, fetchDataRequest } from './countries';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import regionReducer, { fetchAllData, fetchDataFailure, fetchDataRequest, fetchDataSuccess } from './countries';
+
+global.fetch = () => Promise.resolve({
+  json: () => Promise.resolve({
+    dates: {
+      '2022-01-01': {
+        countries: {
+          France: {
+            date: '2022-01-01',
+            id: 'france',
+            name: 'France',
+            regions: [
+              {
+                date: '2022-01-01',
+                id: 'martinique',
+                name: 'Martinique',
+                today_new_confirmed: 284,
+                today_new_deaths: 0,
+              },
+            ],
+            today_new_confirmed: 2356,
+            today_new_deaths: 33,
+          },
+        },
+        info: {
+          date: '2022-01-01 00:00CEST',
+        },
+      },
+    },
+    metadata: {
+      by: 'Narrativa & AppliedXL',
+    },
+    total: {
+      name: 'Total',
+      today_new_confirmed: 676208,
+      today_new_deaths: 3621,
+    },
+    updated_at: '2022-04-14 12:36UTC',
+  }),
+});
+
+const mockStore = configureMockStore([thunk]);
 
 describe('Test for region reducer', () => {
   const initialState = {
@@ -33,5 +76,29 @@ describe('Test for region reducer', () => {
     const newState = regionReducer(currentState, fetchDataFailure());
 
     expect(newState).toEqual(expectedState);
+  });
+
+  it('sets state for successful fetch data action', () => {
+    const expectedState = { status: 'fetched', data: 'blah' };
+
+    const newState = regionReducer(initialState, fetchDataSuccess('blah'));
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it('dispatches request and success action on fetchAllData action', () => {
+    const store = mockStore(initialState);
+
+    store.dispatch(fetchAllData('2022-01-01'))
+      .then(() => {
+        expect(store.getActions()).toEqual([
+          fetchDataRequest(),
+          fetchDataSuccess([{
+            id: 'france',
+            name: 'France',
+            stat: 2356,
+          }]),
+        ]);
+      });
   });
 });
